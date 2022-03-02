ad5f3142eaaf228b39c001ae4a4977ad251b9498
Magda Kovács <kovacsmagda@gmail.com>
2022-03-02 20:06:43 +0100

    script.js update - wrong

--- frontend/script.js
+++ frontend/script.js
@@ -1,84 +1,91 @@
-function Country(name, short, capital, population, flag, continent) {
-    this.name = name;
-    this.short = short;
-    this.capital = capital;
-    this.population = population;
-    this.flag = flag;
-    this.continent = continent;
-}
+function Country(name, short, population, flag, continent) {
+    this.name = name
+    this.short = short
+    this.population = population
+    this.flag = flag
+    this.continent = continent
+};
 
-// Components
-/* const header = (logo) => {
+//Components
+const menuButton = () => {
+    return `
+    <button id="menu-btn">
+        <svg width="40" height="40">
+            <rect width="20" height="2"/>
+            <rect width="20" height="2"/>
+            <rect width="20" height="2"/>
+        </svg>
+        <span>button</span>
+    </button>
+    `;
+};
+
+const header = (logo, button) => {
     return `
     <header>
         <a id="logo">${logo}</a>
-        <button></button>
+        ${button()}
     </header>
-    `
-} */
-
-
-// Code from yesterday
-const countrySection = (h1) => {
-    return `
-    <div>
-        <h1>${h1}</h1>
-    </div>
     `;
-}
+};
 
-
-// 3 Components = HTML elements we would like to add to the document later
+//Exercise
+const countryCards = (cards) => {
+    return `
+    <div id="cards">
+        ${cards}
+    </div>
+    `
+};
 
 const countryCard = (name, short, population, flag, continent) => {
     return `
-        <div class="card">
-            <h1>${country.name}</h1>
-            <p>${country.short}</p>
-            <p>${country.population}</p>
-            <p>${country.continent}</p>
-            <p>"${country.flag}"</p>
-        </div>
-    `;
-}
-
-const countryCards = (contentHTML) => {
-    return`
-        <section class="country-cards">&{contentHTML}</section>
+    <div>
+        <h2>${name}</h2>
+        <h2>${short}</h2>
+        <p>${population}</p>
+        <img src="${flag}"></img>
+        <p>${continent}</p>
+    </div>
     `;
 };
 
 
-const loadEvent = async _ => {
-    // 1. GET DATA
+
+const loadEvent = async () => {
+    //Get data
     // ez a ket sor belement a valtozoba fetchrol jovo adatot, masodik sorban feldolgozzuk
     // await - az egy aszinkron muvelet, megvarjuk a letoltodest
     // fuggveny neve utan ki kell irni, hogy async, innen tudja, hogy meg kell varni bizonyos letolteseket
     // countryArr bevarja h a countryRes leformazodjon
+
     const countryRes = await fetch("https://restcountries.com/v3.1/all");
     const countryArr = await countryRes.json();
-    // console.log(countryArr[0]);
-
-
-    // 2. PROCESS DATA
+    // console.log(countryArr[0])
+    //Process data
     let countries = countryArr.map(function (country) {
-        return new Country(country.name.common, country.cca3, country.population, country.continents[0], country.flags.svg);
-
+        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0])
     })
     console.log(countries);
     //fuggvenyeket elemeket const-tal mentjuk le
+    const rootElement = document.getElementById("root")
+    rootElement.insertAdjacentHTML('beforeend', header("Countries", menuButton));
 
-    let cards = ""
+    //Exercise
+    let content = ""
+    for (let item of countries) {
+        content += countryCard(item.name, item.short, item.population, item.flag, item.continent)
+    }
 
-    for (const country of countries) {
-        cards += countrySection(country.name.common, country.cca3);
-    }  
-    
-    const rootElement = document.getElementById("root");
-    rootElement.insertAdjacentHTML("beforeend", cards);
+    rootElement.insertAdjacentHTML('beforeend', countryCards(content));
 
+    const menuButton1 = document.getElementById("menu-btn");
+    menuButton1.addEventListener('click', (event) => {
+        event.target.classList.toggle("clicked")
+    });
 }
-window.addEventListener("load", loadEvent); 
+
+window.addEventListener('load', loadEvent);
 
 
 
