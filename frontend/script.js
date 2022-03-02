56c7be8e0c737985fa883b05f042b0a52703ec3b
Magda Kovács <kovacsmagda@gmail.com>
2022-03-02 20:08:49 +0100

    Link style.css

--- frontend/script.js
+++ frontend/script.js
@@ -1,93 +1,96 @@
-function Country(name, short, population, flag, continent) {
-    this.name = name
-    this.short = short
-    this.population = population
-    this.flag = flag
-    this.continent = continent
+function Country(name, short, population, flag, continent){
+    this.name = name;
+    this.short = short;
+    this.population = population;
+    this.flag = flag;
+    this.continent = continent;
 };
 
-//Components
-const menuButton = () => {
-    return `
-    <button id="menu-btn">
-        <svg width="40" height="40">
-            <rect width="20" height="2"/>
-            <rect width="20" height="2"/>
-            <rect width="20" height="2"/>
-        </svg>
-        <span>button</span>
-    </button>
+// COMPONENTS
+
+//menu button
+const menuButton = _ => {
+    return`
+        <button id="menuButton">
+            <svg width="40" height="40">
+                <rect width="20" height="5"/>
+                <rect width="20" height="5"/>
+                <rect width="20" height="5"/>
+            </svg>
+            <span>Click</span>
+        </button>
     `;
 };
+
 
 const header = (logo, button) => {
     return `
-    <header>
-        <a id="logo">${logo}</a>
-        ${button()}
-    </header>
-    `;
-};
-
-//Exercise
-const countryCards = (cards) => {
-    return `
-    <div id="cards">
-        ${cards}
-    </div>
+        <header>
+            <a id="logo">${logo}</a>
+            ${button()}
+        </header>    
     `
-};
+}
 
-const countryCard = (name, short, population, flag, continent) => {
+const countryCard = (country) => {
     return `
-    <div>
-        <h2>${name}</h2>
-        <h2>${short}</h2>
-        <p>${population}</p>
-        <img src="${flag}"></img>
-        <p>${continent}</p>
-    </div>
+        <div id="cards">
+            <span>${country.name}</span>
+            <span>${country.short}</span>
+            <span>${country.population}</span>
+            <img src="${country.flag}"></img>
+            <span>${country.continent}</span>
+        </div>
     `;
 };
 
+const countryCards = (contentHTML) => {
+    return `
+    <section class="country-cards">${contentHTML}</sectio>
+    `
+}
 
 
-const loadEvent = async () => {
-    //Get data
+
+
+const loadEvent = async _ => {
+    // GET DATA
+    // 1. GET DATA
     // ez a ket sor belement a valtozoba fetchrol jovo adatot, masodik sorban feldolgozzuk
     // await - az egy aszinkron muvelet, megvarjuk a letoltodest
     // fuggveny neve utan ki kell irni, hogy async, innen tudja, hogy meg kell varni bizonyos letolteseket
     // countryArr bevarja h a countryRes leformazodjon
-
     const countryRes = await fetch("https://restcountries.com/v3.1/all");
     const countryArr = await countryRes.json();
-    // console.log(countryArr[0])
-    //Process data
-    let countries = countryArr.map(function (country) {
+    // console.log(countryArr[0]);
+    // PROCESS DATA
+    let countries = countryArr.map(function(country){
         return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0])
-    })
-    console.log(countries);
+    });
+    // console.log(countries);
     //fuggvenyeket elemeket const-tal mentjuk le
-    const rootElement = document.getElementById("root")
-    rootElement.insertAdjacentHTML('beforeend', header("Countries", menuButton));
-
-    //Exercise
-    let content = ""
-    for (let item of countries) {
-        content += countryCard(item.name, item.short, item.population, item.flag, item.continent)
+    const rootElement = document.getElementById("root");
+    
+    rootElement.insertAdjacentHTML("beforeend", header("Countries", menuButton));
+    
+    let countryHTML = "";
+    for (const country of countries) {
+        countryHTML += countryCard(country)
     }
+    rootElement.insertAdjacentHTML("beforeend", countryCards(countryHTML));
 
-    rootElement.insertAdjacentHTML('beforeend', countryCards(content));
-
-    const menuButton1 = document.getElementById("menu-btn");
-    menuButton1.addEventListener('click', (event) => {
+    const getMenuButton = document.getElementById("menuButton")
+    getMenuButton.addEventListener("click", (event) => {
         event.target.classList.toggle("clicked")
     });
-}
 
-window.addEventListener('load', loadEvent);
+};
 
+window.addEventListener("load", loadEvent);
 
+// Esemeny gyuruzodes svg - click event
+
+// Esemeny gyuruzodes svg - click event
 
 
 // Feladat szunetre - jelenitsuk meg kartyakent ezt a kodot. Country-t. Egy komponensbe mentsuk el a countryCard componensbe, ezt hivjuk meg es iteraljon vegig. 
