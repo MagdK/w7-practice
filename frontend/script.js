16bf7a40ef30881dbda568914fe87b13611fc2fd
MagdK <kovacsmagda@gmail.com>
2022-03-02 14:35:55 +0100

    Calendar work

--- frontend/script.js
+++ frontend/script.js
@@ -1,41 +1,118 @@
-// Programozasban objektum merete nem olyan nagy, de ettol fuggetlenul nagy merete van pl egy tervrajzhoz kepest (hasonlat) - blueprinteket hozunk leszre, tartalommal nem feltoltott valamik. Amikor ezt objektumma formazzuk, ezt a valamit, onnantol kezdve ugy lesz objektum, hogy valami adatot teszunk bele. 
-// Object construction - ez a folyamat, amit odafenn leirtunk a Date szocskaal - constructor
-// A lenyeg az egeszben, hogy mi is letre tudunk hozni objektumokat ugyanarra a semara. 
-// Date az egy building constructor, de mi is tudunk sajatot letrehozni
+// Ezek nagyjabol azok a lepcsok, amik minden feladat vaza - nagy projekteknel
+// Prepare data - 2-es lepes-re epul a legtobb codewars projekt - business logic
 
-/* const currentDate = new Date("1999/01/02")
-
-console.log(currentDate); // console.log(typeof currentDate); => object lesz a typeof-ja */
-
+// 1 Get data
+// 2 Prepare data
+// 3 Components = HTML elements we would like to add to the document later
+// 4 Render = add the components to the document
 
 
 
-
-function Book(title, author, year, genre) {
-    // ez a tervrajz
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.genre = genre;
-    this.age = function() {
-        // ezzel a metodussal alabb a konyv korat megirtuk
-        const d = new Date()
-        const currentYear = d.getFullYear()
-        return currentYear - this.year
-    }
+// 1 Get data
+// ez itt az object constructorunk
+function Month(name, id, nth, days) {
+    this.name = name;
+    this.id = id;
+    this.nth = nth;
+    this.days = days;
 }
 
-const myFavouritBook = new Book('War and peace', 'Tolsztoj', 1867, 'Historical novel');
-console.log(myFavouritBook.title); // => War and peace
-// this szocska minidig objektumra mutat vissza, ha nem, akkor a globalis window objektumra mutat vissza. Ha objektumma valik, ezert mi tudjuk hasznalni a this szot, ezzel utalunk vissza arra hogy az objektum milyen kulcsokat kapjon meg
+const months = [
+    new Month('January', 'jan', '1', '31'),
+    new Month('February', 'feb', '2', '28'),
+    new Month('March', 'mar', '3', '31'),
+    new Month('April', 'apr', '4', '30'),
+    new Month('May', 'may', '5', '31'),
+    new Month('June', 'jun', '6', '30'),
+    new Month('July', 'jul', '7', '31'),
+    new Month('August', 'aug', '8', '31'),
+    new Month('September', 'sep', '9', '30'),
+    new Month('October', 'oct', '10', '31'),
+    new Month('November', 'nov', '11', '30'),
+    new Month('December', 'dec', '12', '31'),
+]
+// console.log(months[0].name);
+// console.log(months);
 
-const mySecondFavouritBook = new Book('Algebra alapjai', 'Joe Algebra', 1992, 'sci-fi');
-console.log(mySecondFavouritBook.age()); // => Algebra alapjai
-
-// Nagyon sok minden construct-orral mukodik. Classok egy masik fele szintaktika. Friss dolgok  a js-en belul, ugyanez a funkcio csak mas a szintaktika. 
-// Objektumokon belul vannak metodusok, itt is az object constructor-on belul is tudunk metodust letrehozni
 
 
+// 2 Prepare data
+// Komponensek azok a dolgok, amik ismetlodnek tobbszor az oldalon es a legkisebb egyseg
+// scope a parameterekenel most function scope alabb(id, h1,days)
+
+const monthSection = (id, h1, days) => {
+    return `
+    <section id="${id}">
+        <h1>${h1}</h1>
+        <div class="days">${days}</div>
+    </section>
+    `;
+}
 
 
-// !!! Menjunk vissza a start coding feladathoz, bankos feladatnal object constructorral hozzuk letre a 3 bank accountot.. Jo feladat lehet gyakorolni ezt a constructort. 
+
+// 3 Components = HTML elements we would like to add to the document later
+
+const dayCard = (year, month, day) => {
+    return `
+    <div class="card">
+        <time>${year}</time>
+        <time>${month}</time>
+        <time>${day}</time>
+        <button class="card-btn">Get day name</button>
+    </div>
+    `
+}
+
+const dayCards = (month, callDayCard) => {
+    let toReturn = "";
+    for (let i = 1; i <= month.days; i++) {
+        // uj fogalom - callback fuggveny - nem ott futtatjuk, ahol szeretnenk hasznalni, hane majd kesobb, amikor itt az ideje lefuttatni
+        toReturn += callDayCard(2022, month.nth, i);
+    }
+    return toReturn;
+}
+// console.log(dayCards(months[0], dayCard));
+
+
+
+
+// 4 Render = add the components to the document
+
+const loadEvent = _ => {
+    // loadevent parametere mar foglalt - akarmit is irunk be, minden esemeny eseteben - ez a parameter csak ezt az egy objectet adja vissza. Benne van a target, path stb. 
+    // az alavonas a () zarojelek helyere - error kezelest igy szoktak megoldani - ebben az esetben az alavonas azt jelenti, hogy mi nem fogjuk hasznalni a parametereket
+    
+    let content = ""
+
+    for (const month of months) {
+        content += monthSection(month.id, month.name, dayCards(month, dayCard));
+    }  
+    document.getElementById("root").insertAdjacentHTML("beforeend", content);
+    
+   /*  function cardButtonClickEvent(event) {
+        console.log(event.target.parentElement);
+        event.target.parentElement.cardList.toggle("clicked")
+    }
+
+    // Click event - event handling
+    const cardList = document.querySelectorAll(".card");
+
+    for (const card of cardList) {
+        // console.log(card);
+        card.querySelector("button").addEventListener("click", cardButtonClickEvent)
+    } */
+    function clickEvent(event){
+        // console.log(event.target);
+        // console.dir(this.target); // => documentre mutat ra
+        if(event.target.classList.contains("card-btn")) {
+            console.log("Hello click");
+            event.target.innerHTML = "This button was clicked"
+        }
+    }
+
+    document.addEventListener("click", clickEvent);
+}
+window.addEventListener("load", loadEvent); // calback fuggveny, nem teszunk zarojelet a vegere, argumentumkent adjuk meg
+
+// feladat szunetre - a script js nek a load eventjebe a rootba a sectionoket rendereljuk bele. a load eventnek nem kell parameter . section-okbe rakjuk bele a daycards componens es mindez keruljon bele a html kodba. 
