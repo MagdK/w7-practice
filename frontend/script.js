1cdc0b24836057f4216bf42946d4354e39f1068a
MagdK <kovacsmagda@gmail.com>
2022-03-02 15:27:01 +0100

    Szunet elott

--- frontend/script.js
+++ frontend/script.js
@@ -1,118 +1,68 @@
-// Ezek nagyjabol azok a lepcsok, amik minden feladat vaza - nagy projekteknel
-// Prepare data - 2-es lepes-re epul a legtobb codewars projekt - business logic
-
-// 1 Get data
-// 2 Prepare data
-// 3 Components = HTML elements we would like to add to the document later
-// 4 Render = add the components to the document
-
-
-
-// 1 Get data
-// ez itt az object constructorunk
-function Month(name, id, nth, days) {
+function Country(name, short, capital, population, flag, continent) {
     this.name = name;
-    this.id = id;
-    this.nth = nth;
-    this.days = days;
+    this.short = short;
+    this.capital = capital;
+    this.population = population;
+    this.flag = flag;
+    this.continent = continent;
 }
 
-const months = [
-    new Month('January', 'jan', '1', '31'),
-    new Month('February', 'feb', '2', '28'),
-    new Month('March', 'mar', '3', '31'),
-    new Month('April', 'apr', '4', '30'),
-    new Month('May', 'may', '5', '31'),
-    new Month('June', 'jun', '6', '30'),
-    new Month('July', 'jul', '7', '31'),
-    new Month('August', 'aug', '8', '31'),
-    new Month('September', 'sep', '9', '30'),
-    new Month('October', 'oct', '10', '31'),
-    new Month('November', 'nov', '11', '30'),
-    new Month('December', 'dec', '12', '31'),
-]
-// console.log(months[0].name);
-// console.log(months);
-
-
-
-// 2 Prepare data
-// Komponensek azok a dolgok, amik ismetlodnek tobbszor az oldalon es a legkisebb egyseg
-// scope a parameterekenel most function scope alabb(id, h1,days)
-
-const monthSection = (id, h1, days) => {
+// Components
+const header = (logo) => {
     return `
-    <section id="${id}">
-        <h1>${h1}</h1>
-        <div class="days">${days}</div>
-    </section>
-    `;
-}
-
-
-
-// 3 Components = HTML elements we would like to add to the document later
-
-const dayCard = (year, month, day) => {
-    return `
-    <div class="card">
-        <time>${year}</time>
-        <time>${month}</time>
-        <time>${day}</time>
-        <button class="card-btn">Get day name</button>
-    </div>
+    <header>
+        <a id="logo">${logo}</a>
+        <button></button>
+    </header>
     `
 }
 
-const dayCards = (month, callDayCard) => {
-    let toReturn = "";
-    for (let i = 1; i <= month.days; i++) {
-        // uj fogalom - callback fuggveny - nem ott futtatjuk, ahol szeretnenk hasznalni, hane majd kesobb, amikor itt az ideje lefuttatni
-        toReturn += callDayCard(2022, month.nth, i);
+const loadEvent = async _ => {
+    // 1. GET DATA
+    // ez a ket sor belement a valtozoba fetchrol jovo adatot, masodik sorban feldolgozzuk
+    // await - az egy aszinkron muvelet, megvarjuk a letoltodest
+    // fuggveny neve utan ki kell irni, hogy async, innen tudja, hogy meg kell varni bizonyos letolteseket
+    // countryArr bevarja h a countryRes leformazodjon
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+    // console.log(countryArr[0]);
+
+
+    // 2. PROCESS DATA
+    let countries = countryArr.map(function (country) {
+        return new Country(country.name.common, country.cca3, country.population, country.continents[0], country.flags.svg);
+
+    })
+    console.log(countries);
+    //fuggvenyeket elemeket const-tal mentjuk le
+    const rootElement = document.getElementById("root");
+    rootElement.insertAdjacentHTML("beforeend", header("Countries"));
+
+    for (const country of countryArr) {
+        //console.log(country.name.common);
+        
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${country.name.common}</h1>
+                <p>${country.cca3}</p>
+                <p>${country.cca3}</p>
+                <p>${country.population}</p>
+                <p>${country.continents[0]}</p>
+                <p>${country.flags.svg}</p>
+            </section>
+        `)
+        
     }
-    return toReturn;
+
 }
-// console.log(dayCards(months[0], dayCard));
+
+window.addEventListener("load", loadEvent); 
+
+// Feladat szunetre - jelenitsuk meg kartyakent ezt a kodot. Country-t. Egy komponensbe mentsuk el a countryCard componensbe, ezt hivjuk meg es iteraljon vegig. 
+// legyen 3-as lepcso mint tegnap - counrtyCard komponenst - countryCards
+
+// div - cards az id
+//countrycard-ba mentett html-t tobbszor rakja be egymas ala
 
 
-
-
-// 4 Render = add the components to the document
-
-const loadEvent = _ => {
-    // loadevent parametere mar foglalt - akarmit is irunk be, minden esemeny eseteben - ez a parameter csak ezt az egy objectet adja vissza. Benne van a target, path stb. 
-    // az alavonas a () zarojelek helyere - error kezelest igy szoktak megoldani - ebben az esetben az alavonas azt jelenti, hogy mi nem fogjuk hasznalni a parametereket
-    
-    let content = ""
-
-    for (const month of months) {
-        content += monthSection(month.id, month.name, dayCards(month, dayCard));
-    }  
-    document.getElementById("root").insertAdjacentHTML("beforeend", content);
-    
-   /*  function cardButtonClickEvent(event) {
-        console.log(event.target.parentElement);
-        event.target.parentElement.cardList.toggle("clicked")
-    }
-
-    // Click event - event handling
-    const cardList = document.querySelectorAll(".card");
-
-    for (const card of cardList) {
-        // console.log(card);
-        card.querySelector("button").addEventListener("click", cardButtonClickEvent)
-    } */
-    function clickEvent(event){
-        // console.log(event.target);
-        // console.dir(this.target); // => documentre mutat ra
-        if(event.target.classList.contains("card-btn")) {
-            console.log("Hello click");
-            event.target.innerHTML = "This button was clicked"
-        }
-    }
-
-    document.addEventListener("click", clickEvent);
-}
-window.addEventListener("load", loadEvent); // calback fuggveny, nem teszunk zarojelet a vegere, argumentumkent adjuk meg
-
-// feladat szunetre - a script js nek a load eventjebe a rootba a sectionoket rendereljuk bele. a load eventnek nem kell parameter . section-okbe rakjuk bele a daycards componens es mindez keruljon bele a html kodba. 
+// ne a load eventen belul legyenek a komponensek ,csak a hozzaadasuk
